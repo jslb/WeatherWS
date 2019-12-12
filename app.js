c3924b8eaa782447ebea4aebb2ba3dd7d8f19ac6
EC2 Default User <ec2-user@ip-172-31-44-201.us-east-2.compute.internal>
2019-12-12 10:11:50 +0000

    complete

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 const request = require('request');
-
 const  bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -14,25 +14,35 @@ app.get('/', function (req, res) {
     res.sendFile('/home/ec2-user/oneRoute/index.html');
 });
 
-
-request('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=93abcd905332612d2a77ca3a4e1b2aa7', function(err, res, body) {
-    console.log(body);
-});
-
-
+global.tempvar = 'Error';
 
 app.post('/submit-location-data', function (req, res) {
     var startVar = req.body.startLocation;
     var daysVar = req.body.numDays;
     var startVar2 = '' + startVar;
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + startVar2 + '&units=metric&appid=93abcd905332612d2a77ca3a4e1b2aa7';
+    var unit = 'metric';
+    var unitString;
+
+    if (daysVar == 'c') {
+      unit = 'metric';
+      unitString = 'celcius';
+    } else if (daysVar == 'f') {
+      unit = 'imperial';
+      unitString = 'farenheit';
+    } else {
+      unit = 'error';
+    }
+    console.log(unit);
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + startVar2 + '&units=' + unit + '&appid=93abcd905332612d2a77ca3a4e1b2aa7';
     console.log(url);
 
     request({url: url}, function(err, res, body) {
-        console.log(body);
+	var results = JSON.parse(body);
+	tempvar = results.main.temp;
     }); 
 
-    res.send('Showing weather for ' + daysVar + ' days, in ' + startVar + 'Temperature:  ' + req.body.name);
+    res.send('Showing weather for ' + unitString + ' days, in ' + startVar + '. Temperature:  ' + tempvar);
 });
 
 var server = app.listen(5000, function () {
